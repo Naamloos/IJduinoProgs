f531821b191e8a09c1a83c8c28696a0b76dd20b7
Ryan de Jonge <ryandejonge@outlook.com>
2015-06-06 23:27:50 +0200

    IT WORKS!!!!! IM DONE YESS :smirk:
    :smirk: :smirk: :smirk: :smirk: :smirk: :smirk: :smirk: :smirk: :smirk:
    :smirk: :smirk: :smirk: :smirk: :smirk: :smirk: :smirk: :smirk: :smirk:
    :smirk: :smirk: :smirk: :smirk: :smirk: :smirk: :smirk: :smirk: :smirk:
    :smirk: :smirk: :smirk: :smirk:

--- Naamloos/Flappy BirDT/FlappybirDT/FlappybirDT.ino
+++ Naamloos/Flappy BirDT/FlappybirDT/FlappybirDT.ino
@@ -4,37 +4,45 @@
 #include "LedControl.h"
 // scroller from FLAPPIJ BIRD. more code wont be used :3
 LedControl lc = LedControl(20, 5, 21, 1);
+int wallx = 7;
 bool jumping;
 bool atwall = false;
 bool atplayer = false;
 bool dead = true;
+int startx = 1;
+int starty = 5;
+int cury ;
+int curx;
+
+bool firstroll = true;
 const byte homemen1[8] PROGMEM = {B00100100,
-                   B00100100,
-                   B00011000,
-                   B00000000,
-                   B00111100,
-                   B01000010,
-                   B11111111,
-                   B10000001
-                  };
+                                  B00100100,
+                                  B00011000,
+                                  B00000000,
+                                  B00111100,
+                                  B01000010,
+                                  B11111111,
+                                  B10000001
+                                 };
 const byte homemen2[8] PROGMEM = {B00100100,
-                   B00100100,
-                   B00100100,
-                   B00100100,
-                   B00011000,
-                   B01111110,
-                   B11111111,
-                   B10000001
-                  };
-                  
-              const byte ded[8] PROGMEM = {B01000010,
-B01000010,
-B01000010,
-B00000000,
-B00000000,
-B01111110,
-B10000001,
-B00000000};
+                                  B00100100,
+                                  B00100100,
+                                  B00100100,
+                                  B00011000,
+                                  B01111110,
+                                  B11111111,
+                                  B10000001
+                                 };
+
+const byte ded[8] PROGMEM = {B01000010,
+                             B01000010,
+                             B01000010,
+                             B00000000,
+                             B00000000,
+                             B01111110,
+                             B10000001,
+                             B00000000
+                            };
 
 void setup() {
   lc.shutdown(0, false);
@@ -47,9 +55,7 @@ void setup() {
 int buttonState = HIGH;
 
 void loop() {
-  bool ended = isdead();
   int reader = digitalRead(9);
-  if(!ended){
   if (!dead) {
     if (reader != buttonState) {
       buttonState = buttonState;
@@ -61,63 +67,110 @@ void loop() {
     wall();
     jumping = false;
     fixplayer;
+    dead = isdead();
   }
   if (dead) {
+    cury = starty;
+    wallx = 7;
     render(homemen1, 150);
     if (reader != buttonState) {
-        lc.clearDisplay(0);
+      lc.clearDisplay(0);
       buttonState = buttonState;
       spawn();
       dead = false;
       return;
     }
-        render(homemen2, 150);
-            if (reader != buttonState) {
-                lc.clearDisplay(0);
+    render(homemen2, 150);
+    if (reader != buttonState) {
+      lc.clearDisplay(0);
       buttonState = buttonState;
       spawn();
       dead = false;
       return;
     }
-  }
-  }
-  if(ended){
-    render(ded,100000);
   }
 }
 
+
 int render(const byte* frame, long delaytime) {
-    lc.clearDisplay(0);
+  lc.clearDisplay(0);
   for (int r = 0; r < 8; r++) {
     lc.setColumn(0, 7 - r, pgm_read_byte(&frame[r]));
   }
   delay(delaytime);
 }
 
-bool firstroll = true;
 bool isdead() {
-  int ply = playery();
-  bool wallplayb = wallplay();
-  if (firstroll = false){
-  if (wallplayb = true) {
-    if (ply = 0) {
+  if (wallx <= 1) {
+    if (cury <= 1) {
       return true;
+      delay(200);
     }
-    if (ply = 1) {
+    if (cury >= 6) {
       return true;
+      delay(200);
     }
-    if (ply = 6) {
-      return true;
+    else {
+      return false;
     }
-    if (ply = 7) {
-      return true;
-    }
-    return false;
   }
-  }
-  firstroll = false;
-    return false;
+  return false;
 }
 
+// Moving code to one script
 
+
+
+void wall() {
+  lc.setLed(0, wallx, 0, LOW);
+  lc.setLed(0, wallx, 1, LOW);
+  lc.setLed(0, wallx, 6, LOW);
+  lc.setLed(0, wallx, 7, LOW);
+  if (wallx > 0) {
+    wallx = wallx - 1;
+  }
+  if (wallx <= 0) {
+    wallx = 7 ;
+  }
+  lc.setLed(0, wallx, 0, HIGH);
+  lc.setLed(0, wallx, 1, HIGH);
+  lc.setLed(0, wallx, 6, HIGH);
+  lc.setLed(0, wallx, 7, HIGH);
+
+  delay(100);
+}
+
+bool jump() {
+  if (cury < 7) {
+    lc.setLed(0, curx, cury, LOW);
+    cury = cury + 1;
+    lc.setLed(0, curx, cury, HIGH);
+    //delay(60);
+  }
+  return true;
+}
+
+void fall() {
+  if (cury > 0 ) {
+    lc.setLed(0, curx, cury, LOW);
+    cury = cury - 1;
+    lc.setLed(0, curx, cury, HIGH);
+    //delay(80);
+  }
+}
+
+void spawn() {
+  lc.setLed(0, startx, starty, HIGH);
+  cury = starty;
+  curx = startx;
+}
+
+void fixplayer() {
+  lc.setLed(0, curx, cury, LOW);
+  lc.setLed(0, curx, cury, HIGH);
+}
+
+int playery() {
+  return cury;
+}
 

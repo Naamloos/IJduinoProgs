a00985aa0e48eb5298411d57cbda4d3dfa0c000a
Ryan de Jonge <ryandejonge@outlook.com>
2015-06-06 14:15:58 +0200

    Cleaning up code, moving around stuff..
    To be honest, i did not know it was possible to call functions from
    other scripts. now i know this, it will be very easy to keep it
    organised imo  :smirk: :+1:

--- Naamloos/Flappy BirDT/FlappybirDT/FlappybirDT.ino
+++ Naamloos/Flappy BirDT/FlappybirDT/FlappybirDT.ino
@@ -1,36 +1,17 @@
-// This game has been built around an early version of
-// "JumpGame" by pixel :)
+/*This game has been built around an early version of
+ "JumpGame" by pixel :) */
+
 #include "LedControl.h"
 LedControl lc = LedControl(20, 5, 21, 1);
-
-int startx = 1;
-int starty = 0;
-int curx ;
-int cury ;
-int jumpHeight = 4;
 bool jumping;
 
-bool jump() {
-  if (cury < 7) {
-    lc.setLed(0, curx, cury, LOW);
-    cury = cury + 1;
-    lc.setLed(0, curx, cury, HIGH);
-    delay(60);
-  }
-  return true;
-}
-
 void setup() {
-  // put your setup code here, to run once:
   lc.shutdown(0, false);
   lc.clearDisplay(0);
   pinMode(11, OUTPUT);
   digitalWrite(11, LOW);
   pinMode(9, INPUT_PULLUP);
-
-  lc.setLed(0, startx, starty, HIGH);
-  cury = starty;
-  curx = startx;
+  spawn();
 }
 
 int buttonState = HIGH;
@@ -40,16 +21,11 @@ void loop() {
   if (reader != buttonState) {
     buttonState = buttonState;
     jumping = jump();
-    //delay(1000);
   }
-  if (cury > 0 ) {
     if (!jumping) {
-      lc.setLed(0, curx, cury, LOW);
-      cury = cury - 1;
-      lc.setLed(0, curx, cury, HIGH);
-      delay(80);
+    fall();
     }
-  }
+
   jumping = false;
 }
 
